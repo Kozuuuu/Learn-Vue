02cba522a78c09039849de8b68c4e830ac56f0f2
kozuuuu <06jaysang2002@gmail.com>
2024-09-20 15:41:46 +0800

    saving changes

--- TestForMoneyTracker/script.js
+++ TestForMoneyTracker/script.js
@@ -1,17 +1,18 @@
-<script>
-  const toggleButton = document.getElementById("nightModeToggle");
-  const icon = document.getElementById("icon");
+const toggleButton = document.getElementById("nightModeToggle");
+const icon = document.getElementById("icon");
 
-  toggleButton.addEventListener("click", function () {
-    document.body.classList.toggle("night-mode");
-    console.log("Night mode class:", document.body.classList.contains("night-mode"));
+toggleButton.addEventListener("click", function () {
+  document.body.classList.toggle("night-mode");
+  console.log(
+    "Night mode class:",
+    document.body.classList.contains("night-mode")
+  );
 
-    if (document.body.classList.contains("night-mode")) {
-      icon.classList.remove("fa-sun");
-      icon.classList.add("fa-moon");
-    } else {
-      icon.classList.remove("fa-moon");
-      icon.classList.add("fa-sun");
-    }
-  });
-</script>
+  if (document.body.classList.contains("night-mode")) {
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+  } else {
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+  }
+});
